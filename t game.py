4a3c9d4d963fbb0551bd93c14d1b032c1009f20f
JensarnePY <jens.arne.hjellnes@gmail.com>
2024-05-26 17:30:16 +0200

    Update t game.py

--- t game.py
+++ t game.py
@@ -1,4 +1,3 @@
-
 import pygame as pg
 import math
 import time
@@ -10,7 +9,6 @@ WINDOW_SIZE = [750, 500]
 screen = pg.display.set_mode(WINDOW_SIZE)
 
 wordl_size = (32,32)
-
 FOV = 70
 camera = [0,-1,0]
 rot = [0,0]
@@ -64,7 +62,7 @@ def update(camera:list, pos:list, angle:list, render: bool, size = 1):
         render[0] = False
     return points, render
 
-def draw(pos, camera:list, points:list, side:list):
+def draw_and_collide(pos, camera:list, points:list, side:list):
     pos2 = [False,0,0,0]
     if camera[0] >= pos[0]+1 and side[0] == False:
         e = pg.draw.polygon(screen, (36,27,40), ((points[1]), (points[2]), (points[6]), (points[5])))
@@ -94,25 +92,22 @@ def draw(pos, camera:list, points:list, side:list):
             pos2[0],pos2[3] = True,-1
     return pos2
 
-def faie(pos: list):
-    c2 = 0
-    for ii in blocks:
-        if [ii[1]+1,ii[2],ii[3]] == pos:
-            blocks[c2][6][0] = False
-        if [ii[1]-1,ii[2],ii[3]] == pos:
-            blocks[c2][6][1] = False
+def draw(pos, camera:list, points:list, side:list):
+         if camera[0] >= pos[0]+1 and side[0] == False:
+             pg.draw.polygon(screen, (36,27,40), ((points[1]), (points[2]), (points[6]), (points[5])))
+         elif camera[0] < pos[0] and side[1] == False:
+             pg.draw.polygon(screen, (43,73,72), ((points[3]), (points[7]), (points[4]), (points[0])))
 
-        if [ii[1],ii[2],ii[3]+1] == pos:
-            blocks[c2][5][4] = False 
-        if [ii[1],ii[2],ii[3]-1] == pos:
-            blocks[c2][6][5] = False 
+         if camera[1] > pos[1]+1 and side[2] == False:
+             pg.draw.polygon(screen, (67,67,67), ((points[7]), (points[6]), (points[2]), (points[3])))
+         elif camera[1] < pos[1] and side[3] == False:
+             pg.draw.polygon(screen, (11,11,11), ((points[0]), (points[1]), (points[5]), (points[4])))
 
-        if [ii[1],ii[2]+1,ii[3]] == pos:
-            blocks[c2][5][2] = False
-        if [ii[1],ii[2]-1,ii[3]] == pos:
-            blocks[c2][6][3] = False
-        c2 += 1
-    
+         if camera[2] >= pos[2]+1 and side[4] == False:
+             pg.draw.polygon(screen, (26,158,44), ((points[4]), (points[5]), (points[6]), (points[7])))
+         elif camera[2] < pos[2] and side[5] == False:
+             pg.draw.polygon(screen, (255,255,255), ((points[0]), (points[1]), (points[2]), (points[3])))
+
 
 blocks = []
 for x in range(wordl_size[0]):
@@ -123,58 +118,46 @@ for x in range(wordl_size[0]):
         for i in range(v, 5):
         
             db = (x+.5-camera[0])*(x+.5-camera[0])+(v+.5-camera[1])*(v+.5-camera[1])+(y+.5-camera[2])*(y+.5-camera[2])
-            blocks.append([db,[x//8,i//8,y//8],x,i,y,[],[False, False, False, False, False, False], [True,True]])
+            blocks.append([db,[x//8,i//8,y//8],x,i,y,[],[False, False, False, False, False, False], [True,True], 0])
 
 chunk = []
 for x in range(0,wordl_size[0],8):
     for y in range(0,wordl_size[1],8):
         for z in range(-1, 1):
-            chunk.append([[x//8,z//8,y//8],True])
-            
+            chunk.append([[x//8,z//8,y//8],True, 0])
             
 for i in blocks:
+    iii = 0
+    for ii in chunk:
+        if ii[0] == i[1]:
+            i[8] =  iii
+        iii += 1
+    
     n = noise.pnoise2(i[2]/15, i[4]/15) * 5
     n = round(n)
-    
     if i[3] == 4:
         i[6][2] = False
     else:
         i[6][2] = True
-    
-        
     if n < i[3]:
         i[6][3] = True
-    
     n = noise.pnoise2(i[2]/15, (i[4]/15)-1/15) * 5
     n = round(n)
-    
     if n <= i[3]:
         i[6][5] = True
-    
     n = noise.pnoise2(i[2]/15, (i[4]/15)+1/15) * 5
     n = round(n)
-    
     if n <= i[3]:
         i[6][4] = True
-        
     n = noise.pnoise2((i[2]/15)-1/15, i[4]/15) * 5
     n = round(n)
-    
     if n <= i[3]:
         i[6][1] = True
-    
     n = noise.pnoise2((i[2]/15)+1/15, i[4]/15) * 5
     n = round(n)
-    
     if n <= i[3]:
         i[6][0] = True
     
-    
-
-
-
-
-
 timer1 = 0
 t = time.time()
 s = 0
@@ -192,7 +175,6 @@ while run == True:
         pg.display.set_caption(f"FPS: {s}      {rot}     {camera}")
         s = 0
     s += 1
-    
     dt = time.time()-q2
     q2 = time.time()
     
@@ -244,40 +226,32 @@ while run == True:
                 rot = [0,0]
             action = []
             
-            
-    if movd[0] == True:
-        for i in blocks:
-            i[0] = (i[2]+.5-camera[0])*(i[2]+.5-camera[0])+(i[3]+.5-camera[1])*(i[3]+.5-camera[1])+(i[4]+.5-camera[2])*(i[4]+.5-camera[2])
-        blocks.sort(reverse=True)
-    
-    
-    
+    blocks.sort(reverse=True)
     for i in chunk:
+      #  i[2] = (i[0][0]+4-camera[0])*(i[0][0]+4-camera[0])+(i[0][1]+4-camera[1])*(i[0][1]+4-camera[1])+(i[0][2]+4-camera[2])*(i[0][2]+4-camera[2])
         points, sht = update(camera,[i[0][0]*8,i[0][1]*8,i[0][2]*8],rot,[False for _ in range(6)], 8)
         
         i[1] = False
         for point in points:
             if point[0] > 0 and point[0] < WINDOW_SIZE[0] and point[1] > 0 and point[1] < WINDOW_SIZE[1]:
                     i[1] = True
-                    
     for i in blocks:
-        for ii in chunk:
-            if ii[0] == i[1]:
-                i[7][1] = ii[1]
-                break
-        if i[7][1] == True:
+        i[7][1] = chunk[i[8]][1]
+        
+        if i[7][1] == True and i[7][0] == True:
+            i[0] = (i[2]+.5-camera[0])*(i[2]+.5-camera[0])+(i[3]+.5-camera[1])*(i[3]+.5-camera[1])+(i[4]+.5-camera[2])*(i[4]+.5-camera[2])
             if movd != [False,False] and i[6] != [True for i in range(6)]:
                 i[5], i[7] = update(camera,[i[2],i[3],i[4]],rot,i[7])
                 v += 1
                 
-            if i[7][0] == True:
-                pos = draw([i[2],i[3],i[4]], camera, i[5], i[6])
-                
-                
+            if i[0] < 200:
+                pos = draw_and_collide([i[2],i[3],i[4]], camera, i[5], i[6])
+            else:
+                draw([i[2],i[3],i[4]], camera, i[5], i[6])
+            
             if mouse.get_pressed()[2] and pos[0] == True and timer1 > .5:
                 timer1 = 0
                 blocks.remove(i)
-                faie([i[2],i[3],i[4]])
             if mouse.get_pressed()[0] and pos[0] == True and timer1 > .5:
                 timer1 = 0
                 blocks.append([0,[0,0,0],i[2]+pos[1],i[3]+pos[2],i[4]+pos[3],[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],[False,False,False,False,False,False],[False,True]])
